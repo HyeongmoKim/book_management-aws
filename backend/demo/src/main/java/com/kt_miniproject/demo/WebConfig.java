565dd3a5a789576c1507b015a080cb9942cbba55
HyeongmoKim <rga6581@gmail.com>
2025-12-22 17:15:13 +0900

    오류 수정

--- backend/demo/src/main/java/com/kt_miniproject/demo/WebConfig.java
+++ backend/demo/src/main/java/com/kt_miniproject/demo/WebConfig.java
@@ -2,7 +2,7 @@
 package com.kt_miniproject.demo;
 
 import org.springframework.web.servlet.config.annotation.*;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
